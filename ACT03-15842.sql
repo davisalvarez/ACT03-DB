f39a377eee4c4c37761bdb75b6c94d70dc3c4f2a
davisalvarez <davisen321@gmail.com>
2020-04-20 17:56:34 -0600

    Add files via upload

---
+++ ACT03-15842.sql
@@ -0,0 +1,16 @@
+/*******************************************************************************
+UVG
+Bases de Datos
+Davis Alvarez- 15842
+Actividad 03
+********************************************************************************/
+
+--Ejercicio 1. Ranking
+
+
+
+--Ejercicio 2. Tendencias
+
+
+
+--Ejercicio 3. Tropicalización
